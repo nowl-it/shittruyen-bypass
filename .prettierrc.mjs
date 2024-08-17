b19402c7715fe25b88be259b85a191404e82c69f
huyhoangg9owl <thh241005@gmail.com>
2024-08-17 17:35:45 +0700

    I wanna cry

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -2,12 +2,12 @@
  * @type {import('prettier').Options}
  */
 export default {
-  printWidth: 80,
-  tabWidth: 2,
-  useTabs: false,
-  semi: false,
-  singleQuote: false,
-  trailingComma: "none",
+  printWidth: 120,
+  tabWidth: 4,
+  useTabs: true,
+  semi: true,
+  singleQuote: true,
+  trailingComma: "all",
   bracketSpacing: true,
   bracketSameLine: true,
   plugins: ["@ianvs/prettier-plugin-sort-imports"],
--- bypass.ts
+++ bypass.ts
@@ -1,20 +1,17 @@
-const { PLASMO_PUBLIC_BYPASS_API } = process.env
+const { PLASMO_PUBLIC_BYPASS_API } = process.env;
 
-if (typeof window !== "undefined" && typeof window.fetch === "function") {
-  const originalFetch = window.fetch
+if (typeof window !== 'undefined' && typeof window.fetch === 'function') {
+	const originalFetch = window.fetch;
 
-  window.fetch = async (...args) => {
-    let [resource, config] = args
-    if (typeof resource === "string") {
-      resource = "${PLASMO_PUBLIC_BYPASS_API}" + resource
-    } else if (resource instanceof Request) {
-      resource = new Request(
-        "${PLASMO_PUBLIC_BYPASS_API}" + resource.url,
-        resource
-      )
-    }
+	window.fetch = async (...args) => {
+		let [resource, config] = args;
+		if (typeof resource === 'string') {
+			resource = '${PLASMO_PUBLIC_BYPASS_API}' + resource;
+		} else if (resource instanceof Request) {
+			resource = new Request('${PLASMO_PUBLIC_BYPASS_API}' + resource.url, resource);
+		}
 
-    const response = await originalFetch(resource, config)
-    return response
-  }
+		const response = await originalFetch(resource, config);
+		return response;
+	};
 }
